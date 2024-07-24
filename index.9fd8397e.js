20814d82bbb9cf71e910c5e5076e713bba56f151
Your_Name Your_Surname <your@email>
2024-07-24 14:31:55 +0300

    make build

---
+++ index.9fd8397e.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",e=>{let o=document.querySelector("footer");"#menu"===window.location.hash?(document.body.classList.add("page__body--with-menu"),o.style.display="none"):(document.body.classList.remove("page__body--with-menu"),o.style.display="")});
+//# sourceMappingURL=index.9fd8397e.js.map
